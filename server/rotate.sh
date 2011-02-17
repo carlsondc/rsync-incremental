666873c4d8cb5f96b7561734eb159477193f147c
Doug Carlson <carlson@cs.jhu.edu>
2011-02-17 10:38:50 -0500

    tweak to get around permissions

--- server/rotate.sh
+++ server/rotate.sh
@@ -57,7 +57,7 @@ then
         if [ $i -ge $MAX_INDEX_ALLOWED ]
         then
             echo "Removing $f"
-            mv $f $INCREMENTALS_DIR/graveyard/$(date +%s)_$(basename $f)
+            mv $f $INCREMENTALS_DIR/dead_$(date +%s)_$(basename $f)
         fi
     done
 fi
