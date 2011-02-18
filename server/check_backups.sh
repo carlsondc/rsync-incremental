0464fb5b54a2a87a76c4f2387f7c69ca90a9074e
Doug Carlson <carlson@cs.jhu.edu>
2011-02-18 08:29:57 -0500

    changed to reflect last-backup directory name

--- server/check_backups.sh
+++ server/check_backups.sh
@@ -23,13 +23,11 @@ echo "Checking backups in $BACKUP_ROOT"
 echo "Units are $elapsedSecondsPerUnit seconds, warning threshold is $warnThresholdUnits (u) = $warnThresholdSeconds (s)"
 for system in $(find $BACKUP_ROOT -maxdepth 1 -mindepth 1 -type d )
 do
-    if [ $(find $system -maxdepth 1 -type l | wc -l) -ne 1 ]
+    if [ $(find $system -maxdepth 1 -name l.0 -type d | wc -l) -ne 1 ]
     then
-        echo "WARNING: Not enough (or too many) ('current' copies) found. Candidates:"
-        find $system -maxdepth 1 -type l
-        echo "---"
+        echo "WARNING: No 1.0 directory (last backup) found."
     else
-        currentDir=$(find $system -maxdepth 1 -type l)
+        currentDir=$(find $system -maxdepth 1 -name 1.0 -type d)
         lastBackup=$(date -r $currentDir +%s)
         secondsElapsed=$(expr $(date +%s) - $lastBackup)
         unitsElapsed=$(expr $secondsElapsed / $elapsedSecondsPerUnit)
